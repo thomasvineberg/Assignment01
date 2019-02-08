f6abf3a2e34808d3fc28880a5fb02b4eba303b57
Thomas <thomas_v16@outlook.com>
2019-02-07 19:34:30 -0800

    Assignment 1
    COMM 644 javascript

---
+++ js/script.js
@@ -0,0 +1,162 @@
+/*eslint-env browser*/
+
+//STEP 1
+
+/* 
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+
+//STEP 2
+
+var numericLiteral = 7.8;
+var stringLiteral = "Great!";
+var booleanLiteral = true;
+var nullLiteral = null;
+
+//STEP 3
+
+var varComplex = 2 * (9 - 8) - 1;
+var varComplex2 = varComplex - varComplex;
+
+//STEP 4
+
+var sFirstName;
+var sLastName;
+var sAddress;
+var sCity;
+var sState;
+var nZipCode;
+var nYourAge;
+var sReferralSource;
+var bMayWeContactYou;
+
+//STEP 5
+
+var sFirstName = "Tom";
+var sLastName = "Vineberg";
+var sAddress = "330 Park Blvd";
+
+var sCity;
+var sState;
+var nZipCode;
+sCity = "San Diego";
+sState = "CA";
+nZipCode = 92101;
+
+var nYourAge = 35, sReferralSource = "SDCCD", bMayWeContactYou = false;
+
+//STEP 6
+
+var nNumberOfStudents = 25;
+window.console.log("There are " + nNumberOfStudents + " students in the class.");
+
+//STEP 7
+
+var bBoolean = false;
+window.console.log("It is " + bBoolean + " to imply that no one could have predicted this disaster.");
+
+var bBoolean2 = true;
+window.console.log(9 + bBoolean2);
+
+//STEP 8 - add backslash to apostrophe
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+
+//STEP 9
+
+var x = null;
+window.console.log(x);
+
+var y;
+window.console.log(y);
+
+//STEP 10
+
+window.console.log(typeof "Bob");
+window.console.log(typeof 87);
+window.console.log(typeof true);
+window.console.log(typeof [5, 6, 7, 8]);
+window.console.log(typeof myLastName);
+
+//STEP 11
+
+window.alert("Hello " + "Tom Vineberg" + ", welcome to the JavaScript class!");
+
+//STEP 12
+
+var name = "Tom Vineberg";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+//STEP 13
+
+var course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");
+
+//STEP 14
+
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+//STEP 15
+
+var name = window.prompt("Please enter your name.");
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+//STEP 16
+
+var course = window.prompt("Please enter the name of your class.");
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+//STEP 17
+
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+
+//STEP 18
+
+var x = 20;
+x = x + 20;
+window.console.log(x);
+
+//STEP 19
+
+var x = 20;
+x = x * 5;
+window.console.log(x);
+
+//STEP 20
+
+var x = 20 % 3;
+x = x / 1;
+window.console.log(x);
+
+//STEP 21
+
+var x = 20;
+var y = 40;
+window.console.log(x > y || x != y);
+
+//STEP 22
+
+var x = 10;
+var y = 12;
+window.console.log(x >= y && y > y - x);
+
+//STEP 23
+
+var widget = new Object();
+window.console.log(typeof widget);
+
+//STEP 24
+
+window.console.log(widget instanceof Object);
+
+//STEP 25
+
+window.console.log(widget instanceof String);
+
+*/
